057f1b9f3a5cb1dab7cc24e853c42006059d31d9
hari hara kumar <hariharakumar5196@gmail.com>
2025-10-07 15:02:28 +0530

    boilerplate

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TablesModule } from './tables/tables.module';
+import { BookingModule } from './booking/booking.module';
+import { MenuModule } from './menu/menu.module';
+import { OrdersModule } from './orders/orders.module';
+import { NotificationModule } from './notification/notification.module';
+import { ReportModule } from './report/report.module';
+
+@Module({
+  imports: [AuthModule, UserModule, TablesModule, BookingModule, MenuModule, OrdersModule, NotificationModule, ReportModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
